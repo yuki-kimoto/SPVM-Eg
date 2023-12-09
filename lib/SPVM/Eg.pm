8b5346e6d82f62679daa3291fd49f9055c654f96
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-12-09 09:38:12 +0900

    First Commit.

---
+++ lib/SPVM/Eg.pm
@@ -0,0 +1,45 @@
+package SPVM::Eg;
+
+our $VERSION = "0.001";
+
+1;
+
+=head1 Name
+
+SPVM::Eg - Short Description
+
+=head1 Description
+
+The Eg class of L<SPVM> has methods to do someting.
+
+=head1 Usage
+
+  use Eg;
+
+=head1 Fields
+
+
+
+=head1 Class Methods
+
+
+
+=head1 Instance Methods
+
+
+
+
+=head1 Repository
+
+
+
+=head1 Author
+
+Yuki Kimoto C<kimoto.yuki@gmail.com>
+
+=head1 Copyright & License
+
+Copyright (c) 2023 Yuki Kimoto
+
+MIT License
+

70f807673d25e6822941e24cf2c0672a94e56316
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-12-09 09:47:02 +0900

    Add Element.

---
+++ lib/SPVM/Eg/Element.pm
@@ -0,0 +1,45 @@
+package SPVM::Eg::Element;
+
+
+
+1;
+
+=head1 Name
+
+SPVM::Eg::Element - Short Description
+
+=head1 Description
+
+The Eg::Element class of L<SPVM> has methods to do someting.
+
+=head1 Usage
+
+  use Eg::Element;
+
+=head1 Fields
+
+
+
+=head1 Class Methods
+
+
+
+=head1 Instance Methods
+
+
+
+
+=head1 Repository
+
+
+
+=head1 Author
+
+Yuki Kimoto C<kimoto.yuki@gmail.com>
+
+=head1 Copyright & License
+
+Copyright (c) 2023 Yuki Kimoto
+
+MIT License
+

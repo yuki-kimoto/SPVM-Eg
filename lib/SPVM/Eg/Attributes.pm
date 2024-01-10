34c00c817580966d1223139941eb38e0bb7dc441
Yuki Kimoto <kimoto.yuki@gmail.com>
2024-01-10 12:42:54 +0900

    Remove the Eg::Attributes class and etc.
      [Incompatible Changes]
        * Remove the Eg::Attributes class.
        * Remove the attributes field from the Eg::Node.
        * Attributes in the Eg::Node is created on demand.

--- lib/SPVM/Eg/Attributes.pm
+++ lib/SPVM/Eg/Attributes.pm
@@ -6,52 +6,9 @@ package SPVM::Eg::Attributes;
 
 =head1 Name
 
-SPVM::Eg::Attributes - Short Description
+SPVM::Eg::Attributes - Attributes
 
 =head1 Description
 
-The Eg::Attributes class in L<SPVM> has methods to do someting.
-
-=head1 Usage
-
-  use Eg::Attributes;
-
-=head1 Details
-
-
-
-=head1 Interfaces
-
-
-
-=head1 Enumerations
-
-
-
-=head1 Fields
-
-
-
-=head1 Class Methods
-
-
-
-=head1 Instance Methods
-
-
-
-
-=head1 Repository
-
-
-
-=head1 Author
-
-Yuki Kimoto C<kimoto.yuki@gmail.com>
-
-=head1 Copyright & License
-
-Copyright (c) 2024 Yuki Kimoto
-
-MIT License
+Removed.
 

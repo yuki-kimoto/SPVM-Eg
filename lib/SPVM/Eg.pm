5ad62be6a6885f351d3ee4f34143b0959fc7b760
Yuki Kimoto <kimoto.yuki@gmail.com>
2024-01-23 15:48:28 +0900

    All tag methods are removed from the Eg class.
      [Incompatible Changes]
        * All tag methods are removed from the Eg class.

--- lib/SPVM/Eg.pm
+++ lib/SPVM/Eg.pm
@@ -16,9 +16,27 @@ The Eg class in L<SPVM> provides components of web platform SPVM Engine.
 
   use Eg;
   
+  my $document = Eg->document;
+  
+  my $div = $document->create_element("div");
+    
+  $div->set_attribute(atrr => "value");
+  
+  $div->set_text("foo");
+  
 =head1 Repository
 
 L<SPVM::Eg - Github|https://github.com/yuki-kimoto/SPVM-Eg>
+
+=head1 Class Methods
+
+=head2 window
+
+  static method window : Eg::Window ();
+
+=head2 document
+
+  static method document : Eg::Document::HTML ();
 
 =head1 Author
 

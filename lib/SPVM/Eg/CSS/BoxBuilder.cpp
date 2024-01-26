1f6ab695d0265040e4e5e426d40d4c5975728b8b
Yuki Kimoto <kimoto.yuki@gmail.com>
2024-01-26 14:47:29 +0900

    Use to_pairs.

--- lib/SPVM/Eg/CSS/BoxBuilder.cpp
+++ lib/SPVM/Eg/CSS/BoxBuilder.cpp
@@ -149,7 +149,12 @@ int32_t SPVM__Eg__CSS__BoxBuilder__build_box_styles(SPVM_ENV* env, SPVM_VALUE* s
   assert(box);
   
   stack[0].oval = obj_node;
-  env->call_instance_method_by_name(env, stack, "style_pairs", 1, &error_id, __func__, FILE_NAME, __LINE__);
+  env->call_instance_method_by_name(env, stack, "style", 1, &error_id, __func__, FILE_NAME, __LINE__);
+  if (error_id) { return error_id; }
+  void* obj_style = stack[0].oval;
+  
+  stack[0].oval = obj_style;
+  env->call_instance_method_by_name(env, stack, "to_pairs", 1, &error_id, __func__, FILE_NAME, __LINE__);
   if (error_id) { return error_id; }
   void* obj_style_pairs = stack[0].oval;
   

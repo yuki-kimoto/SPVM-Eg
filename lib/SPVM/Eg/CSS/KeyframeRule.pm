cad8586a6baa67b0203cab0fbdd2e71f61bdbb4f
Yuki Kimoto <kimoto.yuki@gmail.com>
2024-01-26 09:11:21 +0900

    Add CSS classes.

---
+++ lib/SPVM/Eg/CSS/KeyframeRule.pm
@@ -0,0 +1,57 @@
+package SPVM::Eg::CSS::KeyframeRule;
+
+
+
+1;
+
+=head1 Name
+
+SPVM::Eg::CSS::KeyframeRule - Short Description
+
+=head1 Description
+
+The Eg::CSS::KeyframeRule class in L<SPVM> has methods to do someting.
+
+=head1 Usage
+
+  use Eg::CSS::KeyframeRule;
+
+=head1 Details
+
+
+
+=head1 Interfaces
+
+
+
+=head1 Enumerations
+
+
+
+=head1 Fields
+
+
+
+=head1 Class Methods
+
+
+
+=head1 Instance Methods
+
+
+
+
+=head1 Repository
+
+
+
+=head1 Author
+
+Yuki Kimoto C<kimoto.yuki@gmail.com>
+
+=head1 Copyright & License
+
+Copyright (c) 2024 Yuki Kimoto
+
+MIT License
+

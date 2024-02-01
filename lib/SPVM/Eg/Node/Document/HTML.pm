c99b7ee975a99dc66fdd3a92e35e15af7e9c435f
Yuki Kimoto <kimoto.yuki@gmail.com>
2024-02-01 13:58:30 +0900

    Add the doc of Eg::Node::Document::HTML.

--- lib/SPVM/Eg/Node/Document/HTML.pm
+++ lib/SPVM/Eg/Node/Document/HTML.pm
@@ -6,48 +6,57 @@ package SPVM::Eg::Node::Document::HTML;
 
 =head1 Name
 
-SPVM::Eg::Node::Document::HTML - Short Description
+SPVM::Eg::Node::Document::HTML - Document(HTMLDocument) in JavaScript
 
 =head1 Description
 
-The Eg::Node::Document::HTML class in L<SPVM> has methods to do someting.
+The Eg::Node::Document::HTML class in L<SPVM> represents any HTML web page loaded in the browser and serves as an entry point into the web page's content, which is the DOM tree.
+
+This class is a port of L<Document(HTMLDocument)|https://developer.mozilla.org/en-US/docs/Web/API/Document> in JavaScript.
 
 =head1 Usage
 
-  use Eg::Node::Document::HTML;
+  my $html_document = Eg->implementation->create_html_document("Title");
+  
+  my $body = $html_dobument->body;
 
-=head1 Details
+=head1 Inheritance
 
-
-
-=head1 Interfaces
-
-
-
-=head1 Enumerations
-
-
-
-=head1 Fields
-
-
-
-=head1 Class Methods
-
-
+L<Eg::Node::Document|SPVM::Eg::Node::Document>
 
 =head1 Instance Methods
 
+=head2 create_element
 
+  method create_element : Eg::Node::Element::HTML ($tag_name : string);
 
+Creates the HTML element specified by $tag_name, or an L<Eg::Node::Element::HTML::Unknown> if $tag_name isn't recognized.
 
-=head1 Repository
+For details, see L<Document.createElement|https://developer.mozilla.org/en-US/docs/Web/API/Document/createElement> in JavaScript.
 
+=head2 document_element
 
+  method document_element : Eg::Node::Element::HTML ();
 
-=head1 Author
+Returns the element that is the root(html) element of the document.
 
-Yuki Kimoto C<kimoto.yuki@gmail.com>
+For details, see L<Document.documentElement|https://developer.mozilla.org/en-US/docs/Web/API/Document/documentElement> in JavaScript.
+
+=head2 head
+
+  method head : Eg::Node::Element::HTML ();
+
+Returns the head element of the current document.
+
+For details, see L<Document.head|https://developer.mozilla.org/en-US/docs/Web/API/Document/head> in JavaScript.
+
+=head2 body
+
+  method body : Eg::Node::Element::HTML ();
+
+Returns the body node of the current document.
+
+For details, see L<Document.body|https://developer.mozilla.org/en-US/docs/Web/API/Document/body> in JavaScript.
 
 =head1 Copyright & License
 

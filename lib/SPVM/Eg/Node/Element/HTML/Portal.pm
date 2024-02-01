cf43e2e0a1f079624b532191e1c04896d142a319
Yuki Kimoto <kimoto.yuki@gmail.com>
2024-02-01 15:38:59 +0900

    Add the docs of HTML Element children.

--- lib/SPVM/Eg/Node/Element/HTML/Portal.pm
+++ lib/SPVM/Eg/Node/Element/HTML/Portal.pm
@@ -6,48 +6,11 @@ package SPVM::Eg::Node::Element::HTML::Portal;
 
 =head1 Name
 
-SPVM::Eg::Node::Element::HTML::Portal - Short Description
+SPVM::Eg::Node::Element::HTML::Portal - HTMLPortalElement in JavaScript
 
 =head1 Description
 
-The Eg::Node::Element::HTML::Portal class in L<SPVM> has methods to do someting.
-
-=head1 Usage
-
-  use Eg::Node::Element::HTML::Portal;
-
-=head1 Details
-
-
-
-=head1 Interfaces
-
-
-
-=head1 Enumerations
-
-
-
-=head1 Fields
-
-
-
-=head1 Class Methods
-
-
-
-=head1 Instance Methods
-
-
-
-
-=head1 Repository
-
-
-
-=head1 Author
-
-Yuki Kimoto C<kimoto.yuki@gmail.com>
+The Eg::Node::Element::HTML::Portal class in L<SPVM> represents L<HTMLPortalElement|https://developer.mozilla.org/en-US/docs/Web/API/HTMLPortalElement> in JavaScript
 
 =head1 Copyright & License
 

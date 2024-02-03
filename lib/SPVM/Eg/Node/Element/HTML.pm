db3d2e2f88f1fbce77148f57582c77763ed6aa48
Yuki Kimoto <kimoto.yuki@gmail.com>
2024-02-03 09:24:09 +0900

    Remove get_attribute and set_attribute in Element::HTML.

--- lib/SPVM/Eg/Node/Element/HTML.pm
+++ lib/SPVM/Eg/Node/Element/HTML.pm
@@ -34,24 +34,6 @@ The inline style of an element in the form of a live L<Eg::CSS::StyleDeclaration
 
 For details, see L<HTMLElement.style|https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/style> in JavaScript.
 
-=head1 Instance Methods
-
-=head2 get_attribute
-
-C<method get_attribute : string ($name : string);>
-
-Returns the value of a specified attribute on the element.
-
-For details, see L<Element.getAttribute|https://developer.mozilla.org/en-US/docs/Web/API/Element/getAttribute> in JavaScript.
-
-=head2 set_attribute
-
-C<method set_attribute : void ($name : string, $value : string);>
-
-Sets the value of an attribute on the specified element.
-
-For details, see L<Element.setAttribute|https://developer.mozilla.org/en-US/docs/Web/API/Element/setAttribute> in JavaScript.
-
 =head1 Well Known Child Classes
 
 =over 2
